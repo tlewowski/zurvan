d6e4d672ccd4350816d182e9ecdaa18b27d7d5b6
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2015-09-15 19:33:47 +0200

    Setting up coverage on Travis and CodeClimate)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
 
   // Add the grunt-mocha-test tasks.
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-istanbul');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
   grunt.initConfig({
     // Configure a mochaTest task
@@ -15,9 +21,21 @@ module.exports = function(grunt) {
         },
         src: ['tests/*.js']
       }
+    },
+    clean : [ 'build' ],
+    watch : {
+      files : [ '*.js', 'tests' ],
+      tasks : 'default'
+    },
+    jshint : {
+      files : ['*.js', 'tests' ],
+	},
+    mocha_istanbul: {
+        coverage: {
+            src: 'tests'
+        }
     }
   });
 
-  grunt.registerTask('default', 'mochaTest');
-
+  grunt.registerTask('default', 'mochaTest');  
 };
