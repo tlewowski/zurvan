dab1ff30bbf1993222b08af3cbd893a1c0416097
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2016-04-16 18:03:42 +0200

    Increased coverage - unacceptable clearImmediate without strict config

--- tests/ImmediateCancellingTests.js
+++ tests/ImmediateCancellingTests.js
@@ -1,12 +1,42 @@
 "use strict";
 var assert = require("assert");
 var TimeUnit = require("../TimeUnit");
-var zurvan = require("../zurvan").withDefaultConfiguration({throwOnInvalidClearTimer: true});
+var zurvan = require("../zurvan");
 
 
 describe('zurvan', function() {
   describe('while manages immediates with strict configuration', function() {
 	it('throws on clearing of non-existent immediate', function() {
+	  var immediate;
+	  var called;
+	  return zurvan.interceptTimers({throwOnInvalidClearTimer: true})
+	  .then(function() {
+	    immediate = setImmediate(function() { called = true; });
+	    clearImmediate(immediate);
+		return zurvan.waitForEmptyQueue();
+	  }).then(function() {
+	    assert(!called);
+	  }).then(function() {
+	    return zurvan.releaseTimers();
+	  }).then(function() {
+	    return zurvan.interceptTimers({throwOnInvalidClearTimer: true});
+	  }).then(function() {
+	    clearImmediate(immediate);
+	  }).then(function() {
+	    return zurvan.releaseTimers().then(function() {
+    		throw new Error("Should not be able to clear unexistent immediate");
+		  }, function() {
+		    throw new Error("Should not be able to clear unexistent immediate");
+		  });
+	  }, function(err) {
+	    assert(err instanceof Error);
+		assert(new RegExp("Invalid UID during clearing immediate").test(err.message));
+	    return zurvan.releaseTimers();
+	  });
+	});
+  });
+    describe('while manages immediates without strict configuration', function() {
+	it('ignores clearing of non-existent immediate', function() {
 	  var immediate;
 	  var called;
 	  return zurvan.interceptTimers()
@@ -23,16 +53,9 @@ describe('zurvan', function() {
 	  }).then(function() {
 	    clearImmediate(immediate);
 	  }).then(function() {
-	    return zurvan.releaseTimers().then(function() {
-    		throw new Error("Should not be able to clear unexistent immediate");
-		  }, function() {
-		    throw new Error("Should not be able to clear unexistent immediate");
-		  });
-	  }, function(err) {
-	    assert(err instanceof Error);
-		assert(new RegExp("Invalid UID during clearing immediate").test(err.message));
-	    return zurvan.releaseTimers();
+	    return zurvan.releaseTimers()
 	  });
 	});
   });
+
 });
